6de178357956458488c635cf77aa75c01cbc8c98
o-ov <zhangfan1@live.com>
2023-04-21 15:11:11 +0800

    Update gpt.go

--- gpt/gpt.go
+++ gpt/gpt.go
@@ -197,9 +197,12 @@ func httpStreamRequestCompletions(msg string, runtimes int) (string, error) {
         if err != nil {
             return "", fmt.Errorf("Unmarshal error: %v", err)
         }
-        chunkMessage := collectedChunks.Data.Choices[0].Delta.Content // extract the message
-        fmt.Println("no 202" + chunkMessage)
-        collectedMessages = append(collectedMessages, chunkMessage) // save the message
+        if collectedChunks.Data.Choices[0].Delta.Content != nil{
+            fmt.Println("200+", collectedChunks.Data.Choices[0].Delta.Content)
+            chunkMessage := collectedChunks.Data.Choices[0].Delta.Content // extract the message
+            fmt.Println("no 202" + chunkMessage)
+            collectedMessages = append(collectedMessages, chunkMessage) // save the message
+        }
     }
 
     // print the time delay and text received
